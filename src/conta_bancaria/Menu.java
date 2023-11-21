bc2e970db22935084cba6a44225e89002c012efe
Deborah <deborag.g17@gmail.com>
2023-11-21 02:54:46 -0300

    Conta

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -2,6 +2,7 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
+import conta_bancaria.model.Conta;
 import conta_bancaria.util.Cores;
 
 public class Menu {
@@ -10,6 +11,18 @@ public class Menu {
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
+		
+		// Criar Objetos da Classe conta para teste
+		
+		Conta c1= new Conta(1, 123 ,1 , "Deborah Rodrigues", 100000.0f);
+		c1.visualizar();
+		c1.sacar(1000.00f);
+		c1.visualizar();
+		c1.depositar(5000.00f);
+		c1.visualizar();
+		
+		
+		
 
 		while (true) {
 
---
+++ src/conta_bancaria/model/Conta.java
@@ -0,0 +1,112 @@
+package conta_bancaria.model;
+
+public class Conta {
+	
+	
+
+	    /*Atributos da Classe Conta*/
+	
+	
+	
+		private int numero;
+		private int agencia;
+		private int tipo;
+		private String titular;
+		private float saldo;
+
+		public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+			this.numero = numero;
+			this.agencia = agencia;
+			this.tipo = tipo;
+			this.titular = titular;
+			this.saldo = saldo;
+			
+			
+			
+			
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+		public int getAgencia() {
+			return agencia;
+		}
+
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+
+		public int getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(int tipo) {
+			this.tipo = tipo;
+		}
+
+		public String getTitular() {
+			return titular;
+		}
+
+		public void setTitular(String titular) {
+			this.titular = titular;
+		}
+
+		public float getSaldo() {
+			return saldo;
+		}
+
+		public void setSaldo(float saldo) {
+			this.saldo = saldo;
+		}
+	  
+		
+	public boolean sacar(float valor) {
+		if (this.getSaldo() < valor) {
+			System.out.println("\n Saldo Insuficiente.");
+			return false;
+			
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+		
+	}
+		
+		public void depositar(float valor) {
+			this.setSaldo(this.getSaldo() + valor);
+		}
+		
+		
+		//Método auxiliar visualizar
+		
+		public void visualizar () {
+			String tipo = "";
+			
+			switch(this.tipo) {
+			case 1 -> tipo ="Conta Corrente";
+			case 2 -> tipo ="Conta Poupança";
+			
+			}
+			
+			
+		System.out.println("\n\n*********************************");	
+		System.out.println("Dados da Conta");	
+		System.out.println("**************************************");
+		System.out.println("Número da Conta:" + this.numero);
+		System.out.println("Agência" + this.agencia );
+		System.out.println("Tipo de conta:" + tipo );
+		System.out.println("Titular da conta:" + this.titular );
+		System.out.println("Saldo da Conta:" + this.saldo );
+		
+	}
+	
+	
+
+}
